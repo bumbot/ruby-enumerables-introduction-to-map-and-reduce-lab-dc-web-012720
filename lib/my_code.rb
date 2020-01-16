b98fe5ef3c80e457d28ccdb3a6c22dc42b90b699
Paul Kim <pkim1295@gmail.com>
2020-01-16 06:45:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,7 @@ def map_to_square(source_array)
   new_array
 end
 
-def reduce_to_total(source_array, starting point)
+def reduce_to_total(source_array, starting_point)
   total = 0
   count = starting point
   
