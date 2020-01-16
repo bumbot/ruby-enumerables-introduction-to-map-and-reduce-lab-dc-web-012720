9d4e77a6356cdf0e3ea6826998e839992a3e5eb0
Paul Kim <pkim1295@gmail.com>
2020-01-16 06:42:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,3 +20,14 @@ def map_to_no_chance(source_array)
   end
   new_array
 end
+
+def map_to_double(source_array) 
+  new_array = []
+  count = 0
+  
+  while count < source_array.length do
+    new_array << 2 *source_array[count]
+    count += 1
+  end
+  new_array
+end
