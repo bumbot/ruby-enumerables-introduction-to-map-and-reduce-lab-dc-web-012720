e568bcb9d261c6c8b1ab05c662916d2b16010f83
Paul Kim <pkim1295@gmail.com>
2020-01-16 06:42:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,14 @@ def map_to_negativize(source_array)
   end
   new_array
 end
+
+def map_to_no_chance(source_array)
+  new_array = []
+  count = 0
+  
+  while count < source_array.length do
+    new_array << source_array[count]
+    count += 1
+  end
+  new_array
+end
